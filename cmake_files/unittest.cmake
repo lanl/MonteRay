fb76d469f33ca40a754fda78f5a90d72d074b042
Terry R. Adams <tadams@lanl.gov>
2018-09-20 16:11:15 -0700

    Completely replaced MR's unittest.cmake with MCATKs.
    It was having trouble finding headers for UnitTest++

--- cmake_files/unittest.cmake
+++ cmake_files/unittest.cmake
@@ -4,21 +4,33 @@
 #
 ################################################
 if( NOT UnitTest_LIBRARIES )
+message( "Locating UnitTest++ -- unittest.cmake" )
 
 set( ut_PathSuffixes ${Platform}/${compiler_install_prefix}
                      ${compiler_install_prefix}
                      ${CMAKE_CXX_COMPILER_ID}/${CMAKE_SYSTEM_NAME}
-                     ${CMAKE_CXX_COMPILER_ID} )
+                     ${CMAKE_CXX_COMPILER_ID} 
+                     UnitTest++
+   )
 
 set( _orig_CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_FIND_LIBRARY_SUFFIXES} )
+
+if(DEFINED ENV{UNITTEST_ROOT}) 
+    set( UNITTEST_ROOT $ENV{UNITTEST_ROOT} )
+    message( "-- unittest.cmake -- UnitTest++ root directoy set by environment variable. UNITTEST_ROOT = [ ${UNITTEST_ROOT} ]" )
+else()
+    #TODO: TRA
+    set( UNITTEST_ROOT ${package_dir} )
+    message( "--xx unittest.cmake -- UnitTest++ root directoy set to by default to package_dir. UNITTEST_ROOT = [ ${UNITTEST_ROOT} ]" )
+endif()
+
 if( UNIX )
     # This changes the preferred extension to make .a's the first match
     set( CMAKE_FIND_LIBRARY_SUFFIXES .a ${CMAKE_FIND_LIBRARY_SUFFIXES} )
     find_library( UnitTest_LIBRARIES 
                   NAMES UnitTest++ 
-                  PATHS ${package_dir}/lib
-                        $ENV{UNITTEST_ROOT}/lib
-                        $ENV{UNITTEST_ROOT}/lib/UnitTest++
+                  PATHS ${UNITTEST_ROOT}/lib
+                  PATH_SUFFIXES ${ut_PathSuffixes}
                   DOC "The UnitTest++ library for System: ${CMAKE_SYSTEM_NAME} Compiler: ${CMAKE_CXX_COMPILER}"
                   NO_DEFAULT_PATH )
     # If this isn't erased, it will still appear in the link line as -L.. and in the rpath!!!
@@ -30,18 +42,32 @@ if( WIN32 )
 endif()
 
 if( NOT UnitTest_LIBRARIES )
-    message( FATAL_ERROR "ERROR: UnitTest++ library not found.  In: ${package_dir} under ${ut_PathSuffixes}" )
+    if(NOT DEFINED ENV{UNITTEST_ROOT}) 
+        message( STATUS "-- unittest.cmake -- ERROR:  UnitTest++ library not found.  Consider setting the UNITTEST_ROOT environment variable." )
+    endif()
+    message( FATAL_ERROR "-- unittest.cmake -- ERROR: UnitTest++ library not found. Searched in: ${UNITTEST_ROOT}/lib under ${ut_PathSuffixes}" )
+else()
+    
 endif()
 
 # Restore the original cmake list of library suffixs that will be searched
 set( CMAKE_FIND_LIBRARY_SUFFIXES ${_orig_CMAKE_FIND_LIBRARY_SUFFIXES} )
 
-find_path( UnitTest_INCLUDE_DIRS UnitTest++.h 
-           PATHS ${package_dir}
-                 $ENV{UNITTEST_ROOT}
-           PATH_SUFFIXES include/UnitTest++ )
+set( ut_include_PathSuffixes UnitTest++ )
 
-message( "Found UnitTest++ libraries in : ${UnitTest_LIBRARIES}" )
-message( "Found UnitTest++ headers   in : ${UnitTest_INCLUDE_DIRS}" )
+find_path( UnitTest_INCLUDE_DIRS UnitTest++.h 
+           PATHS ${UNITTEST_ROOT}/include
+           PATH_SUFFIXES ${ut_include_PathSuffixes} 
+         )
+
+if( NOT UnitTest_INCLUDE_DIRS )
+    if(NOT DEFINED ENV{UNITTEST_ROOT}) 
+        message( STATUS "-- unittest.cmake -- ERROR: UnitTest++ include path not found.  Consider setting the UNITTEST_ROOT environment variable." )
+    endif()
+    message( FATAL_ERROR "-- unittest.cmake -- ERROR: UnitTest++ include path not found. Searched in: ${UNITTEST_ROOT}/include under ${ut_include_PathSuffixes}" )
+endif()    
+
+message( "-- unittest.cmake -- Found UnitTest++ libraries in : ${UnitTest_LIBRARIES}" )
+message( "-- unittest.cmake -- Found UnitTest++ include headers in : ${UnitTest_INCLUDE_DIRS}" )
 
 endif()
