38b23f927084851ff730bce1f02fe46ea0f0acfe
Jeremy Sweezy <jsweezy@lanl.gov>
2018-10-19 07:29:28 -0600

    Make finding LNK3DNT and MonteRay test files use environment variables first and explicitly print the needed variables at failure

--- cmake_files/LocatePackagesDir.cmake
+++ cmake_files/LocatePackagesDir.cmake
@@ -79,38 +79,86 @@ endif()
 # |_|_| |_|_|\_\____/ \__,_|_| |_|\__|
 # 
 # ====================================================================
-find_path( lnk3dnt_location 
+
+message( "-- LocatePackagesDir.cmake -- Looking for geometry test files using the following ENVIRONMENT VARIABLES:"  )
+message( "-- LocatePackagesDir.cmake --     MCATK_LNK3DNT " )
+if(DEFINED ENV{MCATK_LNK3DNT}) 
+    find_path( lnk3dnt_location 
+               NAMES godiva.lnk3dnt
+               PATHS ENV MCATK_LNK3DNT
+             )
+    if( lnk3dnt_location )
+       message( "-- LocatePackagesDir.cmake -- MCATK LNK3DNT test directory set by environment variable. MCATK_LNK3DNT = [ $ENV{MCATK_LNK3DNT} ]" )
+    endif()
+endif()
+
+if( NOT EXISTS ${lnk3dnt_location} )
+    message( "-- LocatePackagesDir.cmake --     LNK3DNTDIR  " )
+    if(DEFINED ENV{LNK3DNTDIR}) 
+        find_path( lnk3dnt_location 
+                   NAMES godiva.lnk3dnt
+                   PATHS ENV LNK3DNTDIR
+                 )
+        if( lnk3dnt_location )
+            message( "-- LocatePackagesDir.cmake -- MCATK LNK3DNT test directory set by environment variable. LNK3DNTDIR = [ $ENV{LNK3DNTDIR} ]" )
+        endif()
+   endif()
+endif()
+
+if( NOT EXISTS ${lnk3dnt_location} )
+    message( "-- LocatePackagesDir.cmake --     MONTERAY_TESTFILES_DIR  " )
+    if(DEFINED ENV{MONTERAY_TESTFILES_DIR}) 
+        find_path( lnk3dnt_location 
+                   NAMES godiva.lnk3dnt
+                   PATHS ENV MONTERAY_TESTFILES_DIR
+                 )
+        if( lnk3dnt_location )
+            message( "-- LocatePackagesDir.cmake -- MCATK LNK3DNT test directory set by environment variable. MONTERAY_TESTFILES_DIR = [ $ENV{MONTERAY_TESTFILES_DIR} ]" )
+        endif()
+   endif()
+endif()
+
+if( NOT EXISTS ${lnk3dnt_location} )
+    message( "-- LocatePackagesDir.cmake -- Looking for geometry test files not found using environment variables. " )
+    message( "-- LocatePackagesDir.cmake -- Looking for geometry test files in standard locations. " )
+    find_path( lnk3dnt_location 
            NAMES godiva.lnk3dnt
            PATHS /local
                  /home/xshares/PROJECTS
                  /home/xshares
                  /usr/projects 
-                 /usr/gapps 
-                 ENV MCATK_LNK3DNT
+                 /usr/gapps                 
            PATH_SUFFIXES mcatk/lnk3dnt
-           DOC "Location of numerous geometry link files." 
-           NO_DEFAULT_PATH )
-
-if( NOT EXISTS ${lnk3dnt_location} )
-  # try LNK3DNTDIR environment variable
-  find_path( lnk3dnt_location 
-           NAMES godiva.lnk3dnt
-           PATHS $ENV{LNK3DNTDIR}
            DOC "Location of numerous geometry link files." 
            NO_DEFAULT_PATH )
 endif()
 
 if( lnk3dnt_location )
-    message( "Found link files at [ ${lnk3dnt_location} ]" )
+    message( "-- LocatePackagesDir.cmake -- Found geometry test files at [ ${lnk3dnt_location} ]" )
 #    add_definitions( -DMCATK_LINK_DIR="${lnk3dnt_location}" )
 else()
-    message( FATAL_ERROR "Could not locate link files *** ${lnk3dnt_location}" )
+    message( FATAL_ERROR "-- LocatePackagesDir.cmake -- Could not locate geometry test files.  Consider setting the LNK3DNTDIR environment variable." )
 endif()
 
 
 # MontRayTestFiles
 # ====================================================================
-find_path( MonteRayTestFiles_location 
+message( "-- LocatePackagesDir.cmake -- Looking for MonteRay test files (cross-sections, collisions files, etc.) using the following ENVIRONMENT VARIABLES:"  )
+message( "-- LocatePackagesDir.cmake --     MONTERAY_TESTFILES_DIR " )
+if(DEFINED ENV{MONTERAY_TESTFILES_DIR}) 
+    find_path( MonteRayTestFiles_location 
+               NAMES 1001-70c_MonteRayCrossSection.bin
+               PATHS ENV MONTERAY_TESTFILES_DIR
+             )
+    if( MonteRayTestFiles_location )
+       message( "-- LocatePackagesDir.cmake -- MonteRay test file directory set by environment variable. MONTERAY_TESTFILES_DIR = [ $ENV{MONTERAY_TESTFILES_DIR} ]" )
+    endif()
+endif()
+
+if( NOT EXISTS ${MonteRayTestFiles_location} )
+    message( "-- LocatePackagesDir.cmake -- MonteRay test file directory not found using environment variables. " )
+    message( "-- LocatePackagesDir.cmake -- Looking for MonteRay test file directory in standard locations. " )
+    find_path( MonteRayTestFiles_location 
            NAMES 1001-70c_MonteRayCrossSection.bin
            PATHS /local
                  /home/xshares/PROJECTS
@@ -121,20 +169,12 @@ find_path( MonteRayTestFiles_location
            PATH_SUFFIXES mcatk/MonteRayTestFiles
            DOC "Location of MonteRay Test files." 
            NO_DEFAULT_PATH )
-
-if( NOT EXISTS ${MonteRayTestFiles_location} )
-  # try MONTERAY_TESTFILES_DIR environment variable
-  find_path( MonteRayTestFiles_location 
-           NAMES 1001-70c_MonteRayCrossSection.bin
-           PATHS $ENV{MONTERAY_TESTFILES_DIR}
-           DOC "Location of MonteRay Test files." 
-           NO_DEFAULT_PATH )
 endif()
 
 if( MonteRayTestFiles_location )
-    message( "Found MonteRay test files at [ ${MonteRayTestFiles_location} ]" )
+    message( "-- LocatePackagesDir.cmake -- Found MonteRay test files at [ ${MonteRayTestFiles_location} ]" )
 #    add_definitions( -DMONTERAY_TESTFILES_DIR="${MonteRayTestFiles_location}" )
 else()
-    message( FATAL_ERROR "Could not locate MonteRay test files *** ${MonteRayTestFiles_location}" )
+    message( FATAL_ERROR "-- LocatePackagesDir.cmake -- Could not locate MonteRay test files." )
 endif()
 
