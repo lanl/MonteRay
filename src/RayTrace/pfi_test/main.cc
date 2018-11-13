5c564676333c79cca27d6ae5f0d94bba33ee23c6
Jeremy Sweezy <jsweezy@lanl.gov>
2018-11-13 16:04:10 -0700

    Add parallel test for the RayListController with the next-event estimator

---
+++ src/RayTrace/pfi_test/main.cc
@@ -0,0 +1,40 @@
+#include "MonteRay_unittest.hh"
+#include "MonteRayMultiStream.hh"
+
+#include <iostream>
+
+#include <mpi.h>
+
+int 
+main( int argc, char* argv[] ) {
+
+    MPI_Init(&argc, &argv);
+
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    MonteRay::MultiStream ParOut;
+    if( world_rank == 0 ) {
+        ParOut.setScreen();
+    }
+    UnitTest::TestReporterOstream<MonteRay::MultiStream> reporter( ParOut );
+
+    unsigned int NLocalFailing = RunTests( argc, argv, reporter );
+
+    unsigned int NGlobalFailing = 0;
+
+    MPI_Allreduce(&NLocalFailing, &NGlobalFailing, 1, MPI_UNSIGNED, MPI_SUM,
+            MPI_COMM_WORLD);
+
+    MPI_Finalize();
+
+    if( world_rank == 0 ) {
+        return NGlobalFailing;
+    } else {
+        return 0;
+    }
+
+}
