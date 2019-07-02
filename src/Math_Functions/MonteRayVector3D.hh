91efff79476eff85b0ef600b9434981070d2f579
Tim Burke <tpburke@lanl.gov>
2019-07-02 16:33:16 -0600

    Cleans up MonteRayVector3D.hh
    * CUDA_CALLABLE_MEMBER -> constexpr
    * bracket operator no longer checks if index < 3 at runtime
      * replaced w/ assertion MONTERAY_ASSERT
    * normal and normalize no longer default to strange behavior if
    magnitude < 0
      * previous behavior was just to return the un-normalized result. Now
      an assertion exists to check magnitude in debug mode

--- src/Math_Functions/MonteRayVector3D.hh
+++ src/Math_Functions/MonteRayVector3D.hh
@@ -8,6 +8,7 @@
 #include <tuple>
 
 #include "MonteRayTypes.hh"
+#include "MonteRayAssert.hh"
 
 namespace MonteRay{
 
@@ -23,125 +24,80 @@ class Vector3D
 {
     T elems[ 3 ];
 public:
-    CUDA_CALLABLE_MEMBER
-    Vector3D( void ) {
+    constexpr Vector3D( void ) {
         elems[ 0 ] = T();
         elems[ 1 ] = T();
         elems[ 2 ] = T();
     }
 
-    CUDA_CALLABLE_MEMBER
-    Vector3D( T* init_elems) {
+    constexpr Vector3D( T* init_elems) {
         elems[ 0 ] = init_elems[ 0 ];
         elems[ 1 ] = init_elems[ 1 ];
         elems[ 2 ] = init_elems[ 2 ];
     }
 
-    CUDA_CALLABLE_MEMBER
-    Vector3D( const Vector3D& rhs ) {
+    constexpr Vector3D( const Vector3D& rhs ) {
         elems[ 0 ] = rhs.elems[ 0 ];
         elems[ 1 ] = rhs.elems[ 1 ];
         elems[ 2 ] = rhs.elems[ 2 ];
     }
 
-    CUDA_CALLABLE_MEMBER
-    Vector3D& operator=( const Vector3D& rhs ) {
+    constexpr Vector3D& operator=( const Vector3D& rhs ) {
         elems[ 0 ] = rhs.elems[ 0 ];
         elems[ 1 ] = rhs.elems[ 1 ];
         elems[ 2 ] = rhs.elems[ 2 ];
         return *this;
     }
 
-    CUDA_CALLABLE_MEMBER
-    Vector3D( T value ) {
+    constexpr Vector3D( T value ) {
         elems[ 0 ] = elems[ 1 ] = elems[ 2 ] = value;
     }
 
-    CUDA_CALLABLE_MEMBER
-    Vector3D( T t0, T t1, T t2 ) {
+    constexpr Vector3D( T t0, T t1, T t2 ) {
         elems[ 0 ] = t0;
         elems[ 1 ] = t1;
         elems[ 2 ] = t2;
     }
 
-    CUDA_CALLABLE_MEMBER
-    ~Vector3D() {}
+    constexpr const T& operator[]( unsigned index ) const { MONTERAY_ASSERT(index < 3); return elems[ index ]; }
+    constexpr T& operator[]( unsigned index ) { MONTERAY_ASSERT(index < 3); return elems[ index ]; }
 
-    CUDA_CALLABLE_MEMBER
-    T operator[]( unsigned index ) const {
-        if( index < 3 ) {
-            return elems[ index ];
-        } else {
-            return T();
-        }
+    constexpr const T* data( void ) const { return elems; }
+
+    constexpr Vector3D& normalize( void ) {
+      T mag = magnitude();
+      MONTERAY_ASSERT(mag > 0.0);
+      return (*this) /= magnitude();
     }
 
-    CUDA_CALLABLE_MEMBER
-    T& operator[]( unsigned index ) {
-        if( index < 3 ) {
-            return elems[ index ];
-        } else {
-#ifndef __CUDA_ARCH__
-            throw std::runtime_error("Vector3D::operator[] has thrown an exception. index = > 3");
-#else
-            return elems[ 0 ]; // silence the voices
-#endif
-        }
-
+    constexpr Vector3D normal( void ) const {
+      T mag = magnitude();
+      MONTERAY_ASSERT(mag > 0.0);
+      Vector3D<T> temp = (*this) * (static_cast<T>(1.0)/mag);
+      return temp;
     }
 
-    CUDA_CALLABLE_MEMBER
-    const T* data( void ) const { return elems; }
-
-    CUDA_CALLABLE_MEMBER
-    Vector3D& normalize( void ) {
-        T mag = magnitude();
-        if( mag > 0.0 ) {
-            return (*this) /= magnitude();
-        } else {
-            return *this;
-            //	    	throw std::runtime_error("Vector3D::normalize() has thrown an exception. Magnitude is 0.0");
-        }
-    }
-
-    CUDA_CALLABLE_MEMBER
-    Vector3D normal( void ) const {
-        T mag = magnitude();
-        if( mag > 0.0 ) {
-            Vector3D<T> temp = (*this) * (1/mag);
-            return temp;
-        } else {
-            return *this;
-            //            throw std::runtime_error("Vector3D::normal() has thrown an exception. Magnitude is 0.0");
-        }
-    }
-
-    CUDA_CALLABLE_MEMBER
-    T magnitude( void ) const {
+    constexpr T magnitude( void ) const {
         return std::sqrt( elems[ 0 ] * elems[ 0 ] + elems[ 1 ] * elems[ 1 ] + elems[ 2 ]*elems[ 2 ] );
     }
 
-    CUDA_CALLABLE_MEMBER
-    Vector3D& operator/=( T value ) { (*this) *= 1/value; return *this; }
+    constexpr Vector3D& operator/=( T value ) { (*this) *= 1/value; return *this; }
 
-    CUDA_CALLABLE_MEMBER
-    Vector3D& operator+=( T value ) {
+    constexpr Vector3D& operator+=( T value ) {
         elems[ 0 ] += value;
         elems[ 1 ] += value;
         elems[ 2 ] += value;
         return *this;
     }
 
-    CUDA_CALLABLE_MEMBER
-    Vector3D& operator*=( T value ) {
+    constexpr Vector3D& operator*=( T value ) {
         elems[ 0 ] *= value;
         elems[ 1 ] *= value;
         elems[ 2 ] *= value;
         return *this;
     }
 
-    CUDA_CALLABLE_MEMBER
-    Vector3D operator*( T value ) const {
+    constexpr Vector3D operator*( T value ) const {
         Vector3D<T> tmp(*this);
         tmp.elems[ 0 ] *= value;
         tmp.elems[ 1 ] *= value;
@@ -151,53 +107,45 @@ public:
 
 
     template< typename T1 >
-    CUDA_CALLABLE_MEMBER
-    Vector3D& operator+=( const Vector3D<T1>& rhs) {
+    constexpr Vector3D& operator+=( const Vector3D<T1>& rhs) {
         for( unsigned d=0; d<3; ++d )
             elems[d] += rhs[d];
         return *this;
     }
 
-    CUDA_CALLABLE_MEMBER
-    Vector3D& operator+=( const Vector3D& rhs ) {
+    constexpr Vector3D& operator+=( const Vector3D& rhs ) {
         elems[ 0 ] += rhs.elems[ 0 ];
         elems[ 1 ] += rhs.elems[ 1 ];
         elems[ 2 ] += rhs.elems[ 2 ];
         return *this;
     }
 
-    CUDA_CALLABLE_MEMBER
-    Vector3D& operator-=( const Vector3D& rhs ) {
+    constexpr Vector3D& operator-=( const Vector3D& rhs ) {
         elems[ 0 ] -= rhs.elems[ 0 ];
         elems[ 1 ] -= rhs.elems[ 1 ];
         elems[ 2 ] -= rhs.elems[ 2 ];
         return *this;
     }
 
-    CUDA_CALLABLE_MEMBER
-    Vector3D& operator-=( T Value ) { (*this) += -Value; return *this; }
+    constexpr Vector3D& operator-=( T Value ) { (*this) += -Value; return *this; }
 
-    CUDA_CALLABLE_MEMBER
-    Vector3D operator+( const Vector3D& that ) const {
+    constexpr Vector3D operator+( const Vector3D& that ) const {
         Vector3D<T> temp( *this );
         temp += that;
         return std::move(temp);
     }
 
-    CUDA_CALLABLE_MEMBER
-    Vector3D operator-( const Vector3D& that ) const {
+    constexpr Vector3D operator-( const Vector3D& that ) const {
         Vector3D<T> temp( *this );
         temp -= that;
         return std::move(temp);
     }
 
-    CUDA_CALLABLE_MEMBER
-    T dot( const Vector3D& rhs ) const {
+    constexpr T dot( const Vector3D& rhs ) const {
         return elems[ 0 ] * rhs.elems[ 0 ] + elems[ 1 ] * rhs.elems[ 1 ] + elems[ 2 ] * rhs.elems[ 2 ];
     }
 
-    CUDA_CALLABLE_MEMBER
-    Vector3D cross( const Vector3D& rhs ) const {
+    constexpr Vector3D cross( const Vector3D& rhs ) const {
         Vector3D<T> temp;
         temp[0] = elems[1]* rhs.elems[2] - elems[2]* rhs.elems[1];
         temp[1] = elems[2]* rhs.elems[0] - elems[0]* rhs.elems[2];
@@ -208,51 +156,42 @@ public:
 };
 
 template< typename T>
-CUDA_CALLABLE_MEMBER
-Vector3D<T> operator*( const T& lhs, const Vector3D<T>& rhs)
+constexpr Vector3D<T> operator*( const T& lhs, const Vector3D<T>& rhs)
 { return rhs*lhs;}
 
 template< typename T>
-CUDA_CALLABLE_MEMBER
-T dot( const Vector3D<T>& vec1, const Vector3D<T>& vec2 )
+constexpr T dot( const Vector3D<T>& vec1, const Vector3D<T>& vec2 )
 { return vec1.dot( vec2 ); }
 
 template< typename T>
-CUDA_CALLABLE_MEMBER
-Vector3D<T> cross( const Vector3D<T>& vec1, const Vector3D<T>& vec2 )
+constexpr Vector3D<T> cross( const Vector3D<T>& vec1, const Vector3D<T>& vec2 )
 { return vec1.cross( vec2 ); }
 
 template< typename T>
-CUDA_CALLABLE_MEMBER
-bool operator==( const Vector3D<T>& rhs, const Vector3D<T>& lhs) {
+constexpr bool operator==( const Vector3D<T>& rhs, const Vector3D<T>& lhs) {
     return (rhs[0]==lhs[0]) && (rhs[1]==lhs[1]) && (rhs[2] == lhs[2]);
 }
 
 template< typename T>
-CUDA_CALLABLE_MEMBER
-Vector3D<T> operator/( const Vector3D<T>& v1, const Vector3D<T>& v2) {
+constexpr Vector3D<T> operator/( const Vector3D<T>& v1, const Vector3D<T>& v2) {
     return Vector3D<T>( v1 ) /= v2;
 }
 
 template< typename T>
-CUDA_CALLABLE_MEMBER
-Vector3D<T> operator/( const Vector3D<T>& v1, double factor ) {
+constexpr Vector3D<T> operator/( const Vector3D<T>& v1, double factor ) {
     return Vector3D<T>( v1 ) /= factor;
 }
 
 template< typename T>
-CUDA_CALLABLE_MEMBER
-bool operator!=( const Vector3D<T>& rhs, const Vector3D<T>& lhs) { return !(rhs==lhs); }
+constexpr bool operator!=( const Vector3D<T>& rhs, const Vector3D<T>& lhs) { return !(rhs==lhs); }
 
 template< typename T>
-CUDA_CALLABLE_MEMBER
-std::ostream& operator << ( std::ostream& os, const Vector3D<T>& arg){
+constexpr std::ostream& operator << ( std::ostream& os, const Vector3D<T>& arg){
     os << "( " << arg[0] << ", "<< arg[1] << ", "<< arg[2]<<" )";
     return os;
 }
 
-CUDA_CALLABLE_MEMBER 
-inline auto getDistanceDirection(
+constexpr auto getDistanceDirection(
         const MonteRay::Vector3D<gpuRayFloat_t>& pos,
         const MonteRay::Vector3D<gpuRayFloat_t>& pos2) {
     auto dir = pos2 - pos;
